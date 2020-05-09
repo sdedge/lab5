4a48ed6ca218797d531387c5ee98b5480f02b554
sdedge <46706541+sdedge@users.noreply.github.com>
2020-05-09 10:38:36 +0300

    Initial commit

---
+++ blog/articles/views.py
@@ -0,0 +1,42 @@
+from .models import Article
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.http import Http404
+
+def archive(request):
+	return render(request, 'archive.html', {"posts":Article.objects.all()})
+
+
+def get_article(request, article_id):
+	try:
+		post = Article.objects.get(id=article_id)
+		return render(request, 'article.html', {"post": post})
+	except Article.DoesNotExist:
+		raise Http404
+		
+def create_post(request):
+	if not request.user.is_anonymous:
+		# Здесь будет основной код представления
+		if request.method == "POST":
+		# обработать данные формы, если метод POST
+			form = {'text': request.POST["text"], 'title':request.POST["title"]}
+		# в словаре form будет храниться информация, введенная пользователем
+			if form["text"] and form["title"]:
+				title = form["title"]
+				for article in Article.objects.all():
+					if article.title == title:
+						form['errors'] = u"Статья с таким именем уже существует"
+						return render(request, 'create_post.html',{'form': form})
+		# если поля заполнены без ошибок
+				article = Article.objects.create(text=form["text"],title=form["title"], author=request.user)
+				return redirect('get_article',article_id=article.id)
+		# перейти на страницу поста
+			else:
+		# если введенные данные некорректны
+				form['errors'] = u"Не все поля заполнены"
+				return render(request, 'create_post.html',{'form': form})
+		else:
+		# просто вернуть страницу с формой, если метод GET
+			return render(request, 'create_post.html', {})
+	else:
+		raise Http404
